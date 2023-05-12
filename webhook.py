a5571a955f5caeb359c30bda3e0cb0dc684dc4fd
Matthew W <fatboychummy@gmail.com>
2023-05-11 23:15:51 -0600

    I doubt this will fix the issues I am having

--- webhook.py
+++ webhook.py
@@ -11,40 +11,50 @@ import config
 class regex_action:
     """Holds a regex and runs a function if the regex matches an input string"""
 
-    def __init__(self, regex:str, on_match):
+    def __init__(self, regex:str, on_match, name:str):
         self.regex = regex
         self.on_match = on_match
+        self.name = name
 
     def check(self, input:str):
         match = re.search(self.regex, input)
-        print(f"Test: '{self.regex}' on '{input}'")
         if match:
-            print("Got match!")
+            print(f"Got match ({self.name})!")
             return match
+
+def print_action(regex:str, what_do:str):
+    print(f"  Regex: {regex}\n    Action: {what_do}")
 
 async def main():
     async with aiohttp.ClientSession() as session:
+        print("Connecting to webhook...")
         webhook = Webhook.from_url(config.webhook["url"], session=session)
+        print("Webhook connected.")
         whb = Bridge(webhook)
         
         print("Setting up regexes.")
 
         regexes = []
 
+        print("The following regex actions are being registered:")
+
         async def player_message(match):
             print("Player message, sending...")
             await whb.on_player_message(match.group(1), match.group(2))
-        regexes.insert(0, regex_action(config.webhook["regex"]["player_message"], player_message))
+        regexes.insert(0, regex_action(config.webhook["regex"]["player_message"], player_message, "Player message"))
+        print_action(config.webhook["regex"]["player_message"], "Send messages that players send ingame to Discord.")
 
         async def player_joined(match):
             print("Player joined, sending...")
             await whb.on_player_join(match.group(1))
-        regexes.insert(0, regex_action(config.webhook["regex"]["player_joined"], player_joined))
+        regexes.insert(0, regex_action(config.webhook["regex"]["player_joined"], player_joined, "Player joined"))
+        print_action(config.webhook["regex"]["player_joined"], "Send player join events to Discord.")
 
         async def player_left(match):
             print("Player left, sending...")
             await whb.on_player_leave(match.group(1))
-        regexes.insert(0, regex_action(config.webhook["regex"]["player_left"], player_left))
+        regexes.insert(0, regex_action(config.webhook["regex"]["player_left"], player_left, "Player left"))
+        print_action(config.webhook["regex"]["player_left"], "Send player leave events to Discord.")
 
         print("Done regex stuff.")
 
