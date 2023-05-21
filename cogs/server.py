ae606e1cdbc7a501ba54dfabadda9a7bd03b686c
Matthew W <fatboychummy@gmail.com>
2023-05-20 23:46:45 -0600

    Add typings to server cog.

--- cogs/server.py
+++ cogs/server.py
@@ -23,7 +23,7 @@ class ServerCog(commands.Cog):
     @commands.guild_only()
     @commands.check_any(commands.is_owner(), commands.has_permissions(administrator=True))
     @commands.cooldown(1, 180)
-    async def shutdown(self, ctx):
+    async def shutdown(self, ctx: discord.Interaction) -> None:
         if self.running:
             self.bot.console_pane.send_keys("stop")
             ctx.send("Server is shutting down. Please give it a minute before attempting to start it again.", ephemeral=True)
@@ -36,9 +36,11 @@ class ServerCog(commands.Cog):
     @commands.guild_only()
     @commands.check_any(commands.is_owner(), commands.has_permissions(administrator=True))
     @commands.cooldown(1, 180)
-    async def startup(self, ctx):
+    async def startup(self, ctx: discord.Interaction) -> None:
         if not self.running:
             self.bot.console_pane.send_keys(config.programs["minecraft"])
+            ctx.send("Server is starting up.", ephemeral=True)
+            await time.sleep(60)
             self.running = True
         else:
             ctx.send("Server is currently running.", ephemeral=True)
@@ -46,9 +48,10 @@ class ServerCog(commands.Cog):
     @app_commands.command(name="reboot-schedule", description="Display the automatic restart schedule of the Minecraft server.")
     @commands.guild_only()
     @commands.check_any(commands.is_owner(), commands.has_permissions(administrator=True))
-    async def reboot_schedule(self, ctx):
+    async def reboot_schedule(self, ctx: discord.Interaction) -> None:
         ctx.send(f"Server restarts at {config.server['restart_time']} UTC daily.", ephemeral=True)
     
+    # TODO: Task to detect when the server shuts down (errors or etc)
 
     @tasks.loop(time=config.server["restart_time"])
     async def automatic_restart_task(self):
