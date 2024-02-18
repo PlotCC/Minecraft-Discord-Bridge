d831359422ec73ce56ed1175776657a047a7d765
Matthew W <fatboychummy@gmail.com>
2024-02-17 22:27:39 -0700

    Implementation of backup-now and list-backups

--- cogs/backups.py
+++ cogs/backups.py
@@ -173,15 +173,160 @@ class BackupsCog(commands.Cog):
 
         return False # This should never be reached, but return false just in case cosmic rays hit the server or something.
 
+    def get_backups(self):
+        # 1.a) Get a list of all backups.
+        backups = []
+        for file in os.listdir(config.backups["backup_location"]):
+            if file.endswith(".zip"):
+                backups.append(file)
+                LOG.debug("Found file: " + file)
+        
+        # 1.b) Sort the backups by:
+        #     - Hourly backups, newest first.
+        #     - Daily backups, newest first.
+        #     - Weekly backups, newest first.
+        hourly = []
+        daily = []
+        weekly = []
+        others = []
+
+        for backup in backups:
+            if "hourly" in backup:
+                hourly.append(backup)
+            elif "daily" in backup:
+                daily.append(backup)
+            elif "weekly" in backup:
+                weekly.append(backup)
+            else:
+                others.append(backup)
+        
+        hourly.sort(reverse=True)
+        daily.sort(reverse=True)
+        weekly.sort(reverse=True)
+
+        return hourly, daily, weekly, others
+
+    async def cleanup_backups(self):
+        """
+        Clean up the backup directory, keeping only the specified number of hourly, daily, and weekly backups.
+        """
+
+        LOG.info("Cleaning up backup directory.")
+        
+        # 1.a) Get the backups
+        hourly, daily, weekly, others = self.get_backups()
+
+        LOG.info(f"Found {len(hourly)} hourly backups, {len(daily)} daily backups, and {len(weekly)} weekly backups.")
+        if len(others) > 0:
+            LOG.warn(f"Found {len(others)} other files in the backups folder.")
+
+        # 2) Remove any backups that exceed the maximum number of backups.
+        #     - Hourly backups, keep the newest x backups.
+        #     - Daily backups, keep the newest x backups.
+        #     - Weekly backups, keep the newest x backups.
+
+        # 2.a) Remove hourly backups. If the newest daily backup is older than 
+        # 24 hours, convert the hourly backup to a daily backup instead of 
+        # deleting it.
+        while len(hourly) > config.backups["hourly_backup_count"]:
+            oldest_hourly = hourly.pop()
+            LOG.info(f"Removing hourly backup: {oldest_hourly}")
+            # backup-weekly-%Y-%m-%d-%H-%M-%S.zip
+            if len(daily) == 0 or datetime.datetime.now() - datetime.datetime.strptime(daily[0], 'backup-daily-%Y-%m-%d-%H-%M-%S.zip') > datetime.timedelta(days=1):
+                LOG.info("Actually converting to daily backup.")
+                # Rename the hourly backup to a daily backup.
+                os.rename(os.path.join(config.backups["backup_location"], oldest_hourly), os.path.join(config.backups["backup_location"], oldest_hourly.replace("hourly", "daily")))
+
+                # Add the new daily backup to the list of daily backups. Since
+                # it should now be the newest, we can just insert it at the
+                # beginning of the list.
+                daily.insert(0, oldest_hourly.replace("hourly", "daily"))
+            else:
+                LOG.debug(f"Remove: {os.path.join(config.backups['backup_location'], oldest_hourly)}")
+                # Delete the hourly backup.
+                os.remove(os.path.join(config.backups["backup_location"], oldest_hourly))
+        
+        # 2.b) Remove daily backups. If the newest weekly backup is older than
+        # 7 days, convert the daily backup to a weekly backup instead of
+        # deleting it.
+        while len(daily) > config.backups["daily_backup_count"]:
+            oldest_daily = daily.pop()
+            LOG.info(f"Removing daily backup: {oldest_daily}")
+            if len(weekly) == 0 or datetime.datetime.now() - datetime.datetime.strptime(weekly[0], 'backup-weekly-%Y-%m-%d-%H-%M-%S.zip') > datetime.timedelta(days=7):
+                LOG.info("Actually converting to weekly backup.")
+                # Rename the daily backup to a weekly backup.
+                os.rename(os.path.join(config.backups["backup_location"], oldest_daily), os.path.join(config.backups["backup_location"], oldest_daily.replace("daily", "weekly")))
+
+                # Add the new weekly backup to the list of weekly backups. Since
+                # it should now be the newest, we can just insert it at the
+                # beginning of the list.
+                weekly.insert(0, oldest_daily.replace("daily", "weekly"))
+            else:
+                LOG.debug(f"Remove: {os.path.join(config.backups['backup_location'], oldest_daily)}")
+                # Delete the daily backup.
+                os.remove(os.path.join(config.backups["backup_location"], oldest_daily))
+
+        # 2.c) Remove weekly backups.
+        while len(weekly) > config.backups["weekly_backup_count"]:
+            oldest_weekly = weekly.pop()
+            LOG.info(f"Removing weekly backup: {oldest_weekly}")
+            LOG.debug(f"Remove: {os.path.join(config.backups['backup_location'], oldest_weekly)}")
+            os.remove(os.path.join(config.backups["backup_location"], oldest_weekly))
+        
+    
+    async def digest_backups(self):
+        """
+        Digest the backups and send a message to the notification channel.
+        """
+        hourly, daily, weekly, others = self.get_backups()
+
+        # 3) Send a message to the notification channel with the status of the
+        # backups.
+        try:
+            hourly, daily, weekly, others = self.get_backups()
+
+            embed = discord.Embed(
+                title="Backup Digest",
+                color=0x00ff00,
+                description="An hourly backup just completed, here is the status of the backups. All lists are displayed newest to oldest."
+            )
+
+            if len(hourly) > 0:
+                embed.add_field(name="Hourly", value="```\n" + "\n".join(hourly) + "\n```", inline=False)
+            if len(daily) > 0:
+                embed.add_field(name="Daily", value="```\n" + "\n".join(daily) + "\n```", inline=False)
+            if len(weekly) > 0:
+                embed.add_field(name="Weekly", value="```\n" + "\n".join(weekly) + "\n```", inline=False)
+            if len(others) > 0:
+                embed.add_field(name="Other", value="```\n" + "\n".join(others) + "\n```", inline=False)
+
+            await self.bot.notification_channel.send(
+                embed=embed
+            )
+        except:
+            LOG.error("Failed to send backup digest to notification channel.")
+    
+    async def _auto_backup(self):
+        if (await self.backup_wrapper("hourly")):
+            await self.cleanup_backups()
+            await self.digest_backups()
+        else:
+            await self.bot.notification_channel.send(":x: **Failed to backup server (automatic, hourly)**")
+
     # @app_commands.checks.cooldown(1, 180.0)
 
     @app_commands.command(name="backup-now", description="Backup the minecraft server right now.")
     @app_commands.checks.has_permissions(administrator=True)
-    async def backup_now(self, interaction: discord.Interaction) -> None:
+    async def backup_now(self, interaction: discord.Interaction, fake_hourly: bool=False) -> None:
         """
         Backup the minecraft server right now.
         """
         await interaction.response.defer(thinking=True)
+
+        if fake_hourly:
+            await self._auto_backup()
+            await interaction.followup.send("Fake hourly backup complete.")
+            return
         
         try:
             LOG.info("Manual backup starting...")
@@ -197,13 +342,13 @@ class BackupsCog(commands.Cog):
             # Log stacktrace to console
             LOG.error(traceback.format_exc())
 
-    @app_commands.command(name="restore", description="Restore a backup by its ID.")
+    @app_commands.command(name="restore", description="Restore a backup by its name.")
     @app_commands.describe(
-        id="The ID of the backup to restore."
+        name="The name of the backup to restore."
     )
     @app_commands.checks.cooldown(1, 180.0)
     @app_commands.checks.has_permissions(administrator=True)
-    async def restore(self, interaction: discord.Interaction, id: str) -> None:
+    async def restore(self, interaction: discord.Interaction, name: str) -> None:
         """
         Restore a backup.
         """
@@ -212,14 +357,38 @@ class BackupsCog(commands.Cog):
     
 
     @app_commands.command(name="list-backups", description="List all backups.")
-    @app_commands.checks.cooldown(1, 180.0)
     @app_commands.checks.has_permissions(administrator=True)
     async def list_backups(self, interaction: discord.Interaction) -> None:
         """
         List all backups.
         """
-        await interaction.response.send_message("This command is not yet implemented.", ephemeral=True)
-        # await interaction.response.defer(thinking=True)
+        await interaction.response.defer(thinking=True)
+
+        try:
+            hourly, daily, weekly, others = self.get_backups()
+
+            embed = discord.Embed(
+                title="Backups",
+                color=0x00ff00,
+                description="All lists are displayed newest to oldest."
+            )
+
+            if len(hourly) > 0:
+                embed.add_field(name="Hourly", value="```\n" + "\n".join(hourly) + "\n```", inline=False)
+            if len(daily) > 0:
+                embed.add_field(name="Daily", value="```\n" + "\n".join(daily) + "\n```", inline=False)
+            if len(weekly) > 0:
+                embed.add_field(name="Weekly", value="```\n" + "\n".join(weekly) + "\n```", inline=False)
+            if len(others) > 0:
+                embed.add_field(name="Other", value="```\n" + "\n".join(others) + "\n```", inline=False)
+            
+            await interaction.followup.send(embed=embed)
+        except:
+            await interaction.followup.send("Failed to get the list of backups.")
+    
+    
+    async def on_error(self, event, *args, **kwargs):
+        LOG.error(f"Error in event {event}: {args} {kwargs}")
 
 
     @app_commands.command(name="stop-backups", description="Stop automatic backups.")
@@ -248,31 +417,23 @@ class BackupsCog(commands.Cog):
             return
         self.auto_backup.start()
         await interaction.response.send_message("Automatic backups started.", ephemeral=True)
-
-
-    async def cleanup_backups(self):
-        if True: # TODO: Finish the cleanup function.
-            LOG.info("Cleanup called, but not yet implemented.")
-            return
-        # Step 1: Clean up the backup directory, keeping only the specified number of hourly, daily, and weekly backups.
-        LOG.info("Cleaning up backup directory.")
-        
-        # 1.a) Get a list of all backups.
-        backups = []
-        for file in os.listdir(config.backups["backup_location"]):
-            if file.endswith(".zip"):
-                backups.append(file)
-        
-        # 1.b) Sort the backups by date.
-
     
+    @app_commands.command(name="cleanup-backups", description="Clean up the backup directory. This is mostly for debugging purposes.")
+    @app_commands.checks.has_permissions(administrator=True)
+    async def cleanup_backups_command(self, interaction: discord.Interaction, wipe_others: bool=False) -> None:
+        """
+        Clean up the backup directory.
+        """
+        await interaction.response.defer(thinking=True)
+        await self.cleanup_backups()
+        await interaction.followup.send("Backup directory cleaned up.")
+
     @tasks.loop(hours=1)
     async def auto_backup(self):
         """
         Automatically backup the server.
         """
-        if (await self.backup_wrapper("hourly")):
-            await self.cleanup_backups()
+        await self._auto_backup()
         
 
     @commands.Cog.listener()
